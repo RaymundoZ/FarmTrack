ffaabbb7a4a34c3cbacd061784a16c7dd35e5157
RaymundoZ <andrej.dron2002@gmail.com>
2024-03-06 02:02:51 +0300

    Init commit

---
+++ src/test/java/com/raymundo/farmtrack/FarmTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raymundo.farmtrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmTrackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
